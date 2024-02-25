a9e3f37a835e923cf69e9f252bdf9fecd7e2925e
uuufa <trump@niubi.com>
2024-02-25 23:10:10 +0800

    cmake settings

--- cmake/compiler_options.cmake
+++ cmake/compiler_options.cmake
@@ -2,43 +2,60 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang
     # set(CMAKE_C_FLAGS_RELEASE "-O2 -DNDBUG")
     # set(CMAKE_CXX_FLAGS_RELEASE "-O2 -DNDBUG")
     
-    add_compile_options (-march=native)
+    # add_compile_options (-march=native)
     
-    add_compile_options (-Wall)
-    add_compile_options (-Wextra)
-    add_compile_options (-pedantic)
-    add_compile_options (-Wshadow)
-    add_compile_options (-Wconversion)
-    add_compile_options (-Wcast-qual)                # Cast for removing type qualifiers
-    add_compile_options (-Wfloat-equal)              # Floating values used in equality comparisons
-    add_compile_options (-Wredundant-decls)          # if anything is declared more than once in the same scope
-    add_compile_options (-Wundef)
+    # warnings
+    add_compile_options (-Wall -Wextra -pedantic -Wshadow -Wconversion -Wcast-qual -Wfloat-equal -Wredundant-decls -Wundef)
+    add_link_options (-Wall -Wextra -pedantic -Wshadow -Wconversion -Wcast-qual -Wfloat-equal -Wredundant-decls -Wundef)
+
+
+    # werrors
     
-    add_compile_options (-fopenmp)
     add_compile_options (-ffast-math)
+    add_link_options (-ffast-math)
 
     add_compile_options (-fdiagnostics-color=always)
+    add_link_options (-fdiagnostics-color=always)
     
     # add_compile_options (-fopt-info)
+    # add_link_options (-fopt-info)
+
+    add_compile_options (-fno-semantic-interposition -flto=auto -fuse-linker-plugin -pipe)
+    add_link_options (-fno-semantic-interposition -flto=auto -fuse-linker-plugin -pipe)
+
+    # add_compile_options (-ffat-lto-objects)
+    # add_link_options (-ffat-lto-objects)
 
 
-    # add_compile_options (-fgraphite-identity)
-    # add_compile_options (-floop-nest-optimize)
-    # add_compile_options (-fdevirtualize-at-ltrans)
-    # add_compile_options (-fipa-pta)
-    add_compile_options (-fno-semantic-interposition)
-    add_compile_options (-flto=auto)
-    add_compile_options (-fuse-linker-plugin)
-    add_compile_options (-pipe)
+    # PGO
+    # add_compile_options (-fprofile-generate=pgo_files)
+    # add_link_options (-fprofile-generate=pgo_files)
+
+    # add_compile_options (-fprofile-use=pgo_files)
+    # add_link_options (-fprofile-use=pgo_files)
 
 
+    # add_compile_options (-fno-omit-frame-pointer -fsanitize=undefined)
+    # add_link_options (-fno-omit-frame-pointer -fsanitize=undefined)
     # add_compile_options (-fsanitize=address)
-    # add_compile_options (-fno-omit-frame-pointer)
-    # add_compile_options (-fsanitize=undefined)
-    
-    
-    # add_compile_options (-save-temps=obj)
-    # add_compile_options (-fverbose-asm) # 汇编保留源码
-    # add_compile_options (-masm=intel) # intel汇编
+    # add_link_options (-fsanitize=address)
+    # add_compile_options (-fsanitize=thread)
+    # add_link_options (-fsanitize=thread)
 
+
+    # add_compile_options (-save-temps=obj)
+    # add_compile_options (-fverbose-asm)
+    # add_compile_options (-masm=intel)
+    # add_link_options (-save-temps=obj)
+    # add_link_options (-fverbose-asm)
+    # add_link_options (-masm=intel)
 endif ()
+    
+    
+    
+if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    add_compile_options (-fgraphite-identity -fdevirtualize-at-ltrans -fipa-pta)
+    add_link_options (-fgraphite-identity -fdevirtualize-at-ltrans -fipa-pta)
+    # add_compile_options (-floop-nest-optimize)
+    # add_link_options (-floop-nest-optimize)
+endif()
--- cmake/package.cmake
+++ cmake/package.cmake
@@ -1,2 +1,11 @@
 find_package (Threads REQUIRED)
+find_package (OpenMP REQUIRED)
 find_package (fmt CONFIG REQUIRED)
+find_package (TBB CONFIG REQUIRED COMPONENTS tbb)
+
+find_path (GMP_INCLUDE_DIR NAMES gmp.h)
+find_library (GMP_LIBRARIES NAMES gmp libgmp)
+find_library (GMPXX_LIBRARIES NAMES gmpxx libgmpxx)
+
+# find_path (MARIADB_INCLUDE_DIR NAMES mariadb)
+# find_library (MARIADB_LIBRARIES NAMES mariadb libmariadb)
